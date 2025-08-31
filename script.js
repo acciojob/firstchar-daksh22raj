2a1cdff7cb57ae6b6d5594fd02c002d98901f0d6
daksh22raj <dakshr498@gmail.com>
2025-08-31 16:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstChar(text) {
-  // your code here
+  // Remove spaces from the start of the string
+  let trimmed = text.trimStart();
+
+  // If string becomes empty after trimming → return ""
+  if (trimmed.length === 0) {
+    return "";
+  }
+
+  // Return the first character of trimmed string
+  return trimmed[0];
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+// Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
+
